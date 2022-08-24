bd447d91499e8f14345fcfe927cb7d6407f68f90
Tayfur Erkenci <terkenci@gmail.com>
2022-08-24 23:10:30 +0300

    item's views implemented with hyperloglog

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,12 +1,18 @@
 import { client } from '$services/redis';
 import {
   itemsKey,
-  itemsByViewsKey
+  itemsByViewsKey,
+  itemsViewsKey
 } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-  return Promise.all([
-    client.hIncrBy(itemsKey(itemId), 'views', 1),
-    client.zIncrBy(itemsByViewsKey(), 1, itemId)
-  ]);
+
+  const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+
+  if(inserted){
+    return Promise.all([
+      client.hIncrBy(itemsKey(itemId), 'views', 1),
+      client.zIncrBy(itemsByViewsKey(), 1, itemId)
+    ]);
+  }
 };
